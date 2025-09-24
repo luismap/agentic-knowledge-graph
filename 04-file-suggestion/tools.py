2970c1cd5a41c8855308246f999e6acb70c61acf
Luis arcia <luismap2508@gmail.com>
2025-09-24 10:17:36 +0200

    [feat] file suggeston agent setup

--- 04-file-suggestion/tools.py
+++ 04-file-suggestion/tools.py
@@ -3,7 +3,13 @@ from google.adk.tools import ToolContext
 from neo4j_for_adk import tool_success, tool_error
 
 def get_approved_user_goal(tool_context: ToolContext):
-    """Returns the user's goal, which is a dictionary containing the kind of graph and its description."""
+    """
+    Returns the user's goal, which is a dictionary containing the kind of graph and its description.
+    Args:
+        no arguments will be pass
+    Returns:
+        tool_sucess: a dictionary
+    """
     if "approved_user_goal" not in tool_context.state:
         return tool_error("approved_user_goal not set. Ask the user to clarify their goal (kind of graph and description).")  
     
